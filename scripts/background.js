6c6795491a502691a2f02894f24cd0a550cd6e15
Toteya <111172685+Toteya@users.noreply.github.com>
2025-02-05 13:10:21 +0200

    update service-worker to use static rules instead of dynamic rules; added on click event listener to allow the user to enable and disable the extension

--- scripts/background.js
+++ scripts/background.js
@@ -1,23 +1,35 @@
-chrome.declarativeNetRequest.updateDynamicRules({
-  addRules: [
-    {
-      id: 1,
-      priority: 1,
-      action: { type: "block" },
-      condition: {
-        urlFilter: "*://*doubleclick.net/*",
-        resourceTypes: ["script", "image", "sub_frame"],
-      }
-    },
-    {
-      id: 2,
-      priority: 1,
-      action: { type: "block" },
-      condition: {
-        urlFilter: "*://*googleadservices.com/*",
-        resourceTypes: ["script", "image", "sub_frame"],
-      }
-    },
-  ],
-  removeRuleIds: [1, 2],
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({ enabled: false }, () => {
+    chrome.action.setBadgeText({
+      text: "OFF",
+    });
+  });
+});
+
+
+async function updateStaticRules(enableRulesetIds, disableRulesetIds) {
+  await chrome.declarativeNetRequest.updateEnabledRulesets({
+      enableRulesetIds: enableRulesetIds,
+      disableRulesetIds: disableRulesetIds
+  });
+}
+
+chrome.action.onClicked.addListener(async () => {
+  await chrome.storage.local.get(['enabled'], async (result) => {
+    enabled = !(result.enabled);
+    chrome.storage.local.set({ enabled: enabled})
+    
+    // update static rules
+    if (enabled) {
+      await updateStaticRules(["ruleset_1"], []);
+    } else {
+      await updateStaticRules([], ["ruleset_1"]);
+    }
+
+    // update the badge status
+    const state = enabled ? '' : 'OFF';
+    chrome.action.setBadgeText({
+      text: state,
+    });
+  })
 });
